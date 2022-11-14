070ab4ab3b2c3cdf04d60b0258fb24cce0dda605
khushi3502 <99663343+khushi3502@users.noreply.github.com>
2022-11-14 14:22:40 +0530

    vscode
    Here I had Created Beginner level Chat bot

---
+++ response.js
@@ -0,0 +1,11 @@
+const responseObj = {
+    hello: "Hey ,how can I help you ?",
+    hey: "Hey! how can I help you",
+    Interestedindoingcourse:"Which course u are interested ?",
+    9234567812: "we will contact you soon",
+    Datascience:"Please share your What'app no?",
+    WebDevlopment:"Please share your NAME , EMAIL-ID , What'app no?",
+    Thankyou: "Be sure to ask if any other question?",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };
